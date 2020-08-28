ae3fea24a838c575b7aacf0a38e26b2ef7dffbd0
Priyali-7 <65449259+Priyali-7@users.noreply.github.com>
2020-08-28 19:05:48 +0530

    Update urls.py

--- order/urls.py
+++ order/urls.py
@@ -6,7 +6,10 @@ urlpatterns=[
     path('services', views.services, name="services"),
     path('contact', views.contact, name='contact'),
     path('menu', views.menu, name='menu'),
-    path('contact_form', views.contact_form, name='contact_form')
+    path('contact_form', views.contact_form, name='contact_form'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout')
 
 
 ]
